f7f232fe94956c76b8d0d6112a6aa5c790201ebe
CRaLFa <minami.smd@gmail.com>
2024-09-14 23:28:13 +0900

    change source of informations

---
+++ disclosure.ts
@@ -0,0 +1,96 @@
+import { DOMParser, Element } from 'jsr:@b-fuze/deno-dom';
+
+type Entry = {
+  time: string;
+  stockCode: string;
+  companyName: string;
+  title: string;
+  url: string;
+};
+
+type Disclosure = {
+  latestItemTime: number;
+  entries: Entry[];
+};
+
+const TDNET_BASE_URL = 'https://www.release.tdnet.info/inbs';
+
+const getTime = (tr: Element) => {
+  const tdTime = tr.querySelector('td.kjTime');
+  return tdTime ? tdTime.textContent.trim() : '';
+};
+
+const getCode = (tr: Element) => {
+  const tdCode = tr.querySelector('td.kjCode');
+  return tdCode ? tdCode.textContent.trim().slice(0, 4) : '';
+};
+
+const getName = (tr: Element) => {
+  const tdName = tr.querySelector('td.kjName');
+  return tdName ? tdName.textContent.trim() : '';
+};
+
+const getTitleAndUrl = (tr: Element) => {
+  const aTitle = tr.querySelector('td.kjTitle > a');
+  return aTitle ? [aTitle.textContent.trim(), aTitle.getAttribute('href') ?? ''] : ['', ''];
+};
+
+const getNumYmd = (d: Date) => d.getFullYear() * 10000 + (d.getMonth() + 1) * 100 + d.getDate();
+
+const getNumHm = (tr: Element) => parseInt(getTime(tr).replace(':', ''), 10);
+
+const getSlashYmd = (d: Date) => {
+  const strYmd = String(getNumYmd(d));
+  return `${strYmd.slice(0, 4)}/${strYmd.slice(4, 6)}/${strYmd.slice(6)}`;
+};
+
+const toEntry = (tr: Element) => {
+  const [title, url] = getTitleAndUrl(tr);
+  const entry: Entry = {
+    time: `${getSlashYmd(new Date())} ${getTime(tr)}`,
+    stockCode: getCode(tr),
+    companyName: getName(tr),
+    title,
+    url: `${TDNET_BASE_URL}/${url}`,
+  };
+  return entry;
+};
+
+const searchDisclosure = async (lastTime: number, searchWords: string[]): Promise<Disclosure> => {
+  const lastYmd = Math.floor(lastTime / 10000), lastHm = lastTime % 10000;
+  const today = getNumYmd(new Date());
+  const isNewItem = (tr: Element) => lastYmd < today || lastHm < getNumHm(tr);
+  const disclosure: Disclosure = {
+    latestItemTime: 0,
+    entries: [],
+  };
+  let page = 0;
+  while (true) {
+    page++;
+    const res = await fetch(`${TDNET_BASE_URL}/I_list_${String(page).padStart(3, '0')}_${today}.html`);
+    if (!res.ok) {
+      return disclosure;
+    }
+    const doc = new DOMParser().parseFromString(await res.text(), 'text/html');
+    const rows = Array.from(doc.querySelectorAll('#main-list-table tr'));
+    if (rows.length < 1) {
+      return disclosure;
+    }
+    if (page === 1) {
+      disclosure.latestItemTime = today * 10000 + getNumHm(rows[0]);
+    }
+    const matchedEntries = rows.filter((row) => {
+      if (!isNewItem(row)) {
+        return false;
+      }
+      const title = getTitleAndUrl(row)[0];
+      return searchWords.some((word) => title.includes(word));
+    }).map(toEntry);
+    disclosure.entries.push(...matchedEntries);
+    if (!isNewItem(rows.at(-1)!)) {
+      return disclosure;
+    }
+  }
+};
+
+export { searchDisclosure };
--- main.ts
+++ main.ts
@@ -1,19 +1,11 @@
-import 'https://deno.land/std@0.218.2/dotenv/load.ts';
-import { createBot, startBot, Intents, ChannelTypes } from 'https://deno.land/x/discordeno@18.0.1/mod.ts';
+import 'jsr:@std/dotenv/load';
+import { basename, extname } from 'jsr:@std/path';
+import { ChannelTypes, createBot, FileContent, Intents, startBot } from 'https://deno.land/x/discordeno@18.0.1/mod.ts';
+import { searchDisclosure } from './disclosure.ts';
 
-const KV_KEY = ['MINKABU-NEWS', 'biz-alliance', 'news-no'];
-
-type SearchResult = {
-  latestNewsNo: number;
-  matchedNews: {
-    no: number;
-    title: string;
-    published_at: string;
-  }[];
-};
+const KV_KEY = ['TDnet', 'biz-alliance', 'lastTime'];
 
 (() => {
-
   if (!Deno.env.has('BOT_TOKEN')) {
     console.error("Environment variable 'BOT_TOKEN' is not set");
     Deno.exit(1);
@@ -27,45 +19,74 @@ type SearchResult = {
   const getTextChannelIds = async (guildIds: bigint[]) => {
     const channelCollections = await Promise.all(guildIds.map((guildId) => bot.helpers.getChannels(guildId)));
     const channels = channelCollections.flatMap((collection) => [...collection.values()]);
-    return channels.filter((chan) => chan.type === ChannelTypes.GuildText && chan.name === '一般').map((chan) => chan.id);
+    return channels.filter((chan) => chan.type === ChannelTypes.GuildText && chan.name === '一般').map((chan) =>
+      chan.id
+    );
   };
 
-  const searchNews = async (lastNewsNo: number, searchWords: string[]) => {
-    const cmd = new Deno.Command('bash', {
+  const getFileContent = async (url: string): Promise<FileContent | undefined> => {
+    const res = await fetch(url);
+    if (!res.ok) {
+      return undefined;
+    }
+    const fileName = basename(url);
+    const data = await res.blob();
+    if (data.type !== 'application/pdf') {
+      return {
+        blob: data,
+        name: fileName,
+      };
+    }
+    const cmd = new Deno.Command('pdftoppm', {
       args: [
-        // '-x',
-        './search_news.sh',
-        String(lastNewsNo),
-        ...searchWords,
+        '-png',
+        '-',
       ],
+      stdin: 'piped',
+      stdout: 'piped',
+      stderr: 'piped',
       signal: AbortSignal.timeout(15000),
     });
-    const { code, stdout, stderr } = await cmd.output();
-    const decoder = new TextDecoder();
+    const process = cmd.spawn();
+    const w = process.stdin.getWriter();
+    w.write(await data.bytes());
+    w.releaseLock();
+    await process.stdin.close();
+    const { code, stdout, stderr } = await process.output();
     if (code !== 0) {
-      console.error(decoder.decode(stderr));
-      return null;
+      console.error(new TextDecoder().decode(stderr));
+      return {
+        blob: data,
+        name: fileName,
+      };
     }
-    return JSON.parse(decoder.decode(stdout)) as SearchResult;
+    return {
+      blob: new Blob([stdout]),
+      name: fileName.replace(extname(fileName), '.png'),
+    };
   };
 
-  const processNews = async (channelIds: bigint[]) => {
+  const processDisclosures = async (channelIds: bigint[]) => {
     const kv = await Deno.openKv();
     // await kv.delete(KV_KEY);
-    const lastNewsNo = (await kv.get<number>(KV_KEY)).value ?? 0;
-    const res = await searchNews(lastNewsNo, ['提携', '協業']);
-    if (!res)
-      return;
-    await kv.set(KV_KEY, res.latestNewsNo);
-    if (res.matchedNews.length < 1) {
+    const lastTime = (await kv.get<number>(KV_KEY)).value ?? 0;
+    const disclosure = await searchDisclosure(lastTime, ['提携', '協業']);
+    if (disclosure.latestItemTime > 0) {
+      await kv.set(KV_KEY, disclosure.latestItemTime);
+    }
+    if (disclosure.entries.length < 1) {
       console.log('No new entry about business alliances');
       return;
     }
-    console.log(JSON.stringify(res));
-    for (const news of res.matchedNews) {
-      const content = `${news.title} (${news.published_at})\nhttps://minkabu.jp/news/${news.no}`;
-      for (const channelId of channelIds)
-        await bot.helpers.sendMessage(channelId, { content });
+    console.log(JSON.stringify(disclosure));
+    for (const entry of disclosure.entries) {
+      const content = `【${entry.companyName} (${entry.stockCode})】${entry.title} (${entry.time})\n${entry.url}`;
+      for (const channelId of channelIds) {
+        await bot.helpers.sendMessage(channelId, {
+          content,
+          file: await getFileContent(entry.url),
+        });
+      }
     }
   };
 
@@ -77,11 +98,10 @@ type SearchResult = {
     startBot(bot);
   }).then(async (guildIds) => {
     const channelIds = await getTextChannelIds(guildIds);
-    Deno.cron('Fetch news', { minute: { every: 1 } }, () => processNews(channelIds));
+    Deno.cron('Fetch disclosures', { minute: { every: 1 } }, () => processDisclosures(channelIds));
     // Deno.exit(0);
   }).catch((err) => {
     console.error(err);
     Deno.exit(1);
   });
-
 })();
